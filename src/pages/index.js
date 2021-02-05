ef620fc3810b76fc1670fca8da1fe93068a466f3
Ryan MacInnes <ryan.macinnes@gmail.com>
2021-02-05 14:11:30 -0800

    Adding covid warning.

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,14 @@ const IndexPage = () => (
         <area shape="rect" coords="703,114,944,183" href="/contact" alt="Contact Link" />
         <area shape="rect" coords="702,211,943,278" href={allForms} alt="Forms Link" />
       </map>
+
+      <div className={style.covid}>
+      	We are pleased to announce that are entire staff has been fully vaccinated against Covid-19.
+	Our office follows CDC guidelines regarding safe practices regarding the Covid virus.
+	We clean all surfaces and testing materials regularly and we maintain social distancing guidelines at all times.
+	We require all patients and their families to wear face masks while in our office.
+	We urge everyone to get a Covid-19 vaccine when it is available to you.
+      </div>
     </div>
     <Markdown path='/info' />
   </Layout>
